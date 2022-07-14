fc8fc4f480f4492bdb9c8e527108e9fa830dad66
Simone Porreca <porrecasimone@gmail.com>
2022-07-14 21:51:02 +0200

    Update utils.py

---
+++ src/utils/utils.py
@@ -0,0 +1,13 @@
+# Import Standard Modules
+
+def read_configuration(file_name):
+    """
+
+    :param file_name:
+    :return:
+    """
+
+    try:
+        
+        # Read configuration file
+        with open('../../configuration/' + file_name)
