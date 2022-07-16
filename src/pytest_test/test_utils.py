f61fb9d2867d1f48bc9f7947c084dcd930883a41
Simone Porreca <porrecasimone@gmail.com>
2022-07-16 22:21:28 +0200

    Create test_utils.py

---
+++ src/pytest_test/test_utils.py
@@ -0,0 +1,14 @@
+# Import Standard Modules
+import os
+import pytest
+
+from src.pytest_test.test_utils_fixtures import test_data_preparation
+
+
+def test_environment_variable(test_data_preparation):
+    """
+
+    :return:
+    """
+    print('Current Dir')
+
