3f3616fff3b53c7de9a64c7b9d9703550156821b
Simone Porreca <porrecasimone@gmail.com>
2022-07-20 17:58:18 +0200

    Create test_scripts.py

---
+++ utils_scripts/test_scripts.py
@@ -0,0 +1,15 @@
+# Import Standard Modules
+import os
+
+# Set root path
+os.chdir(os.environ['CUSTOMER_PERSONALITY_PATH'])
+
+# Import Package Modules
+
+
+def main():
+
+    print('Current Directory')
+    
+
+main()
