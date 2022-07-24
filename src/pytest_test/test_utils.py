e9d25c7583400c3afa4f86c15ca0905611516d92
Simone Porreca <porrecasimone@gmail.com>
2022-07-24 18:08:48 +0200

    Update test_utils.py

--- src/pytest_test/test_utils.py
+++ src/pytest_test/test_utils.py
@@ -21,7 +21,7 @@ def test_environment_variable(test_data_preparation):
 @pytest.mark.parametrize('test_config_file, test_config, expected_value', [
     ('config.yaml', 'data_path', './data/marketing_campaign.csv')
 ])
-def test_read_configuration():
+def test_read_configuration(test_config_file, ):
     """
     Test the function src.utils.utils.read_configuration
     :return: Boolean
