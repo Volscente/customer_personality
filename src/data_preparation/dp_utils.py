ac50770aea3830afe89b2dfe7f89b83573717940
Simone Porreca <porrecasimone@gmail.com>
2022-07-26 17:21:18 +0200

    DP - Add read_data

---
+++ src/data_preparation/dp_utils.py
@@ -0,0 +1,39 @@
+# Import Standard Libraries
+import os
+import sys
+import pandas as pd
+
+# Set root path
+os.chdir(os.environ['CUSTOMER_PERSONALITY_PATH'])
+
+# Import Package Modules
+from src.logging_module.logging_module import get_logger
+
+# Setup logger
+logger = get_logger(os.path.basename(__file__).split('.')[0])
+
+
+def read_data(data_path: str,
+              data_separator: str,
+              data_encoding: str) -> pd.DataFrame:
+    """
+    Read data from 'data_path' location
+    :param data_path: String data location
+    :param data_separator: String data CSV separator
+    :param data_encoding: String data CSV file encoding
+    :return: Pandas DataFrame of read data
+    """
+
+    logger.info('read_data - Start')
+
+    try:
+
+        logger.info('read_data - Read data from {}'.format(data_path))
+
+    except Exception as e:
+
+        logger.error('read_data - Unable to read data from {}'.format(data_path))
+        logger.error(e)
+        sys.exit(1)
+
+
