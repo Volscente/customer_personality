de40d02ff1492b968b528ce3bc766ecedf17de61
Simone Porreca <porrecasimone@gmail.com>
2022-07-26 16:50:31 +0200

    DP - Add logs

--- src/data_preparation/data_preparation.py
+++ src/data_preparation/data_preparation.py
@@ -8,6 +8,10 @@ os.chdir(os.environ['CUSTOMER_PERSONALITY_PATH'])
 
 # Import Package Modules
 from src.utils.utils import read_configuration
+from src.logging_module.logging_module import get_logger
+
+# Setup logger
+logger = get_logger(os.path.basename(__file__).split('.')[0])
 
 
 class DataPreparation:
@@ -16,6 +20,8 @@ class DataPreparation:
         """
         Initialize a DataPreparation object for preparing and cleaning the data to the modeling phase
         """
+
+        logger.info('__init__ - Read configuration file')
 
         # Read Configuration file
         self.config = read_configuration(configuration_file)
@@ -28,6 +34,8 @@ class DataPreparation:
         Run a pipeline for preparing and cleaning the data
         :return: Write prepared data as a .CSV file
         """
+
+        logger.info('run - Running the pipeline')
 
         # Read data
         pass
