f98376b95ef599efeb051852521c33dbdecbcd08
Simone Porreca <porrecasimone@gmail.com>
2022-07-27 10:04:13 +0200

    Pytest - Setup config fixture

--- src/pytest_test/test_data_preparation_fixtures.py
+++ src/pytest_test/test_data_preparation_fixtures.py
@@ -1,2 +1,15 @@
 # Import Standard Modules
 import pytest
+import os
+
+# Set root path
+os.chdir(os.environ['CUSTOMER_PERSONALITY_PATH'])
+
+
+@pytest.fixture
+def test_configuration() -> dict:
+    """
+    Test configuration object dictionary
+    :return: Dictionary configuration
+    """
+
