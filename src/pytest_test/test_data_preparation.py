54de00b66a3ceba42025fbc1c98b6d76010f3878
Simone Porreca <porrecasimone@gmail.com>
2022-07-28 20:28:34 +0200

    Pytest - Define test_read_data

--- src/pytest_test/test_data_preparation.py
+++ src/pytest_test/test_data_preparation.py
@@ -9,8 +9,29 @@ from src.data_preparation.dp_utils import read_data
 from src.pytest_test.test_data_preparation_fixtures import test_configuration
 
 
-def test_read_data(test_configuration: dict):
+@pytest.mark.parametrize('test_id, expected_year_birth, expected_education', [
+    (5524, 1957, 'Graduation'),
+    (2174, 1954, 'Graduation'),
+    (5324, 1981, 'PhD')
+])
+def test_read_data(test_configuration: dict,
+                   test_id: int,
+                   expected_year_birth: int,
+                   expected_education: str):
+    """
+    Test the function src.data_preparation.dp_utils.read_data
+    :param test_configuration: Dictionary configuration object
+    :param test_id: Integer id of the record to test
+    :param expected_year_birth: Integer year_birth of the record to test
+    :param expected_education: String education of the record to test
+    """
 
+    # Read data
     data = read_data(test_configuration['data_path'],
                      test_configuration['data_separator'],
                      test_configuration['data_encoding'])
+
+    # Select record
+    record = data[data['ID'] == test_id][['Year_Birth', 'Education']]
+
+    assert record['Year_Birth'] == expected_year_birth and record['Education'] == expected_education
